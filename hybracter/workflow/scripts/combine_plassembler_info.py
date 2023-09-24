dafc8287c6c397e83e3bb1d6db2320d3012bc6d3
gbouras13 <george.bouras@adelaide.edu.au>
2023-09-25 00:16:50 +0930

    fix plassembler info plus environment file

--- hybracter/workflow/scripts/combine_plassembler_info.py
+++ hybracter/workflow/scripts/combine_plassembler_info.py
@@ -17,7 +17,11 @@ def combine_sample_plassembler(summary_dir, output):
     # write all the summary dfs to a list
     summaries = []
 
+    # counts the number of summaries
     complete_sample_no = len(summary_list)
+
+    # counts the number of samples with plasmids
+    samples_with_plasmids = 0
 
     if complete_sample_no > 0:
         for a in summary_list:
@@ -25,12 +29,14 @@ def combine_sample_plassembler(summary_dir, output):
             if os.path.getsize(a) > 0:
                 tmp_summary = pd.read_csv(a, delimiter="\t", index_col=False, header=0)
                 summaries.append(tmp_summary)
+                # appends to the number of plasmids
+                samples_with_plasmids += 1
 
-    if complete_sample_no > 1:
+    if samples_with_plasmids > 1:
         # make into combined dataframe
         total_summary_df = pd.concat(summaries, ignore_index=True)
         total_summary_df.to_csv(output, sep=",", index=False)
-    elif complete_sample_no == 1:  # just print the first
+    elif samples_with_plasmids == 1:  # just print the first
         summaries[0].to_csv(output, sep=",", index=False)
     else:  # touch the output
         with open(output, "a"):
