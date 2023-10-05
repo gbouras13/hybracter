c069cbf475068cb7263c6c554c1144bc3ec27a5f
gbouras13 <george.bouras@adelaide.edu.au>
2023-10-05 12:35:04 +1030

    add tests end to end for all commands

--- tests/test_hybracter.py
+++ tests/test_hybracter.py
@@ -12,6 +12,9 @@ __type__ = "Test Script"
 __maintainer__ = "George Bouras"
 __email__ = "george.bouras@adelaide.edu.au"
 
+# test data
+test_data_path = Path("hybracter/test_data")
+db_dir: Path = "plassembler_db"
 
 
 @pytest.fixture(scope="session")
@@ -48,7 +51,7 @@ def exec_command(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
 
 
 def test_hybracter_hybrid():
-    """test hybracter hybrid"""
+    """hybracter test-hybrid"""
     threads = 4
     outdir: Path = "test_hybracter_output"
     cmd = f"hybracter test-hybrid --threads {threads} --output {outdir} --no_polca"
@@ -57,7 +60,7 @@ def test_hybracter_hybrid():
 
 
 def test_hybracter_long():
-    """test hybracter long"""
+    """hybracter test-long"""
     threads = 4
     outdir: Path = "test_hybracter_output"
     cmd = f"hybracter test-long --threads {threads} --output {outdir}"
@@ -66,18 +69,55 @@ def test_hybracter_long():
 
 
 def test_hybracter_install():
-    """test hybracter install"""
-    outdir: Path = "plassembler_db"
-    cmd = f"hybracter install  --databases {outdir}"
+    """test hybracter install and install dirs for later tests"""
+    cmd = f"hybracter install  --databases {db_dir}"
+    exec_command(cmd)
+
+
+def test_hybracter_hybrid_csv():
+    """test hybracter hybrid"""
+    threads = 4
+    outdir: Path = "test_hybracter_output"
+    input_csv: Path = test_data_path / "test_hybrid_input.csv"
+    cmd = f"hybracter hybrid --input {input_csv} --threads {threads} --output {outdir} --no_polca --databases {db_dir}"
     exec_command(cmd)
     remove_directory(outdir)
 
+
+def test_hybracter_long():
+    """test hybracter long"""
+    threads = 4
+    outdir: Path = "test_hybracter_output"
+    input_csv: Path = test_data_path / "test_long_input.csv"
+    cmd = f"hybracter long --input {input_csv} --threads {threads} --output {outdir} --databases {db_dir}"
+    exec_command(cmd)
+    remove_directory(outdir)
+
+def test_hybracter_hybrid_single():
+    """test hybracter hybrid w"""
+    threads = 4
+    outdir: Path = "test_hybracter_output"
+    longreads: Path = test_data_path / "Fastqs/test_long_reads.fastq.gz"
+    reads1: Path = test_data_path / "Fastqs/test_short_reads_R1.fastq.gz"
+    reads2: Path = test_data_path / "Fastqs/test_short_reads_R2.fastq.gz"
+    cmd = f"hybracter hybrid-single -l {longreads} -1 {reads1} -2 {reads2} -c 50000 -s Sample1 --threads {threads} --output {outdir} --no_polca --databases {db_dir}"
+    exec_command(cmd)
+    remove_directory(outdir)
+
+
+def test_hybracter_long_single():
+    """test hybracter long"""
+    threads = 4
+    outdir: Path = "test_hybracter_output"
+    longreads: Path = test_data_path / "Fastqs/test_long_reads.fastq.gz"
+    cmd = f"hybracter long-single -l {longreads} -c 50000 -s Sample1  --threads {threads} --output {outdir} --databases {db_dir}"
+    exec_command(cmd)
+    remove_directory(outdir)
 
 def test_citation():
     """test hybracter citation"""
     cmd = "hybracter citation"
     exec_command(cmd)
-
 
 def test_version():
     """test hybracter version"""
@@ -96,3 +136,6 @@ class errors(unittest.TestCase):
             cmd = f"hybracter test-hybrid --threads {threads} --output {outdir} --database {empty_db} --no_polca"
             exec_command(cmd)
             remove_directory(outdir)
+
+# cleanup 
+remove_directory(db_dir)
